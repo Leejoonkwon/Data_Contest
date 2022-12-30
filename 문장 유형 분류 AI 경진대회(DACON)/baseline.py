a0e22ea7532482988cb76368b336414292540753
Leejoonkwon <qnfk7895@naver.com>
2022-12-30 10:01:34 +0900

    20221230

--- 문장 유형 분류 AI 경진대회(DACON)/baseline.py
+++ 문장 유형 분류 AI 경진대회(DACON)/baseline.py
@@ -87,3 +87,34 @@ class SentenceTypeDataset(Dataset):
             return text, torch.Tensor(type_tmp), torch.Tensor(polarity_tmp), torch.Tensor(tense_tmp), torch.Tensor(certainty_tmp)
         else:
             return text, torch.Tensor([-1,-1,-1,-1]), torch.Tensor([-1,-1,-1]), torch.Tensor([-1,-1,-1]), torch.Tensor([-1,-1])
+        
+class SentenceClassifier(nn.Module):
+    def __init__(self, base_model):
+        super().__init__()
+        self.klue = base_model # from transformers package
+
+        self.fc1 = nn.Linear(768, 32)
+        self.relu = nn.ReLU()
+        self.type_clf = nn.Linear(32,4)
+        self.polarity_clf = nn.Linear(32,3)
+        self.tense_clf = nn.Linear(32,3)
+        self.certainty_clf = nn.Linear(32,2)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_ids, attention_mask):
+        # input_ids : token's id / attention_mask : make a model to focus on which token
+        klue_out = self.klue(input_ids= input_ids, attention_mask = attention_mask)[0][:,0]
+
+        x = self.fc1(klue_out)
+        x = self.relu(x)
+
+        type_output = self.type_clf(x)
+        type_output = self.softmax(type_output)
+        polarity_output = self.polarity_clf(x)
+        polarity_output = self.softmax(polarity_output)
+        tense_output = self.tense_clf(x)
+        tense_output = self.softmax(tense_output)
+        certainty_output = self.certainty_clf(x)
+        certainty_output = self.softmax(certainty_output)
+
+        return type_output, polarity_output, tense_output, certainty_output
