d7e37c24c930fef4538c014b4892a23ac4f2768e
Leejoonkwon <qnfk7895@naver.com>
2022-12-30 10:01:17 +0900

    20221230

--- 문장 유형 분류 AI 경진대회(DACON)/baseline.py
+++ 문장 유형 분류 AI 경진대회(DACON)/baseline.py
@@ -58,3 +58,32 @@ sns.histplot(tokenizer_len)
 plt.show()
 
 print(f'log value : {np.mean(tokenizer_len)+3*np.std(tokenizer_len)}')
+
+tokenizer_log = np.log(tokenizer_len)
+sns.histplot(tokenizer_log)
+plt.show()
+
+print(f'log value : {np.mean(tokenizer_log)+3*np.std(tokenizer_log)}')
+print(f'original value : {np.exp(np.mean(tokenizer_log)+3*np.std(tokenizer_log))}')
+
+class SentenceTypeDataset(Dataset):
+    def __init__(self, dataframe, tokenizer, labels=None):
+        texts = dataframe['문장'].values.tolist()
+
+        self.texts = [tokenizer(text, padding='max_length', max_length=90, truncation=True, return_tensors='pt') for text in texts]
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+
+        if self.labels is not None:
+            type_tmp = self.labels['type'][idx]
+            polarity_tmp = self.labels['polarity'][idx]
+            tense_tmp = self.labels['tense'][idx]
+            certainty_tmp = self.labels['certainty'][idx]
+            return text, torch.Tensor(type_tmp), torch.Tensor(polarity_tmp), torch.Tensor(tense_tmp), torch.Tensor(certainty_tmp)
+        else:
+            return text, torch.Tensor([-1,-1,-1,-1]), torch.Tensor([-1,-1,-1]), torch.Tensor([-1,-1,-1]), torch.Tensor([-1,-1])
