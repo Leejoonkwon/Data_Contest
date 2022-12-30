22537409fcd2b0b033bb849b3ae66e72f13e3c0c
Leejoonkwon <qnfk7895@naver.com>
2022-12-30 10:00:54 +0900

    20221230

--- 문장 유형 분류 AI 경진대회(DACON)/baseline.py
+++ 문장 유형 분류 AI 경진대회(DACON)/baseline.py
@@ -1,20 +1,60 @@
-import random
+# https://dacon.io/en/competitions/official/236037/overview/description
+
 import pandas as pd
 import numpy as np
-import os
-
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import preprocessing
-from sklearn.metrics import f1_score
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+import os
+import random
+from sklearn.model_selection import train_test_split
+from transformers import AutoModel, AutoTokenizer
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch import nn
+from tqdm import tqdm
 
-from tqdm.auto import tqdm
+# for graphing
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings(action='ignore') 
+path = 'C:\data_set\\text_dacon/'
+train_original = pd.read_csv(path + 'train.csv')
+train_original.drop(columns=['ID'], inplace=True)
+test = pd.read_csv(path + 'test.csv')
+test.drop(columns=['ID'], inplace=True)
+submission = pd.read_csv(path + 'sample_submission.csv')
+
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+CFG = {
+    'EPOCHS':20,
+    'LEARNING_RATE':1e-5,
+    'BATCH_SIZE':32,
+    'SEED':41
+}
+
+seed_everything(CFG['SEED']) # Seed 고정
+device = torch.device('mps')
+
+train, val, _, _ = train_test_split(train_original, train_original['label'], test_size=0.2, random_state=CFG['SEED'])
+train = train.reset_index(drop=True)
+val = val.reset_index(drop=True)
+
+model_nm = 'klue/roberta-small'
+base_model = AutoModel.from_pretrained(model_nm)
+tokenizer = AutoTokenizer.from_pretrained(model_nm)
+
+tokenizer_len = [len(tokenizer(s)['input_ids']) for s in train['문장']]
+sns.histplot(tokenizer_len)
+plt.show()
+
+print(f'log value : {np.mean(tokenizer_len)+3*np.std(tokenizer_len)}')
